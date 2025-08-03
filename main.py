9ca4632421055079dc2205bdd8d13ff39c2272ef
Runner89 <alain.login@hotmail.com>
2025-08-03 10:37:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 #    "secret_key": "",
 #    "symbol": "BABY-USDT",
 #    "position_side": "LONG",
-#    "sell_percentage": 1.5,
+#    "sell_percentage": 2.5,
 #    "price": 0.068186,
 #    "leverage": 1,
 #    "FIREBASE_SECRET": "",
@@ -178,15 +178,6 @@ def place_limit_sell_order(api_key, secret_key, symbol, quantity, limit_price, p
 
     response = requests.post(url, headers=headers, json=params_dict)
     return response.json()
-
-def firebase_speichere_alarmwert(asset, wert, firebase_secret):
-    url = f"{FIREBASE_URL}/alarmwerte/{asset}.json?auth={firebase_secret}"
-    data = {"alarmwert": wert}
-    response = requests.put(url, json=data)
-    if response.status_code == 200:
-        return f"Alarmwert für {asset} gespeichert: {wert}"
-    else:
-        return f"Fehler beim Speichern des Alarmwerts für {asset}: {response.status_code} - {response.text}"
     
 def sende_telegram_nachricht(text):
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
