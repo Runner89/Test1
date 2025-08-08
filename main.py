cc0ed7d75a6a9cafbf1490d4e1fb007de4409bd1
Runner89 <alain.login@hotmail.com>
2025-08-08 19:07:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -610,7 +610,8 @@ def webhook():
         "usdt_balance_before_order": available_usdt,
         "stop_loss_price": stop_loss_price if liquidation_price else None,
         "stop_loss_response": stop_loss_response if liquidation_price else None,
-        "saved_usdt_amount": saved_usdt_amounts,      
+        "saved_usdt_amount": saved_usdt_amounts,
+        "status_fuer_alle": status_fuer_alle,
         "logs": logs
     })
 
