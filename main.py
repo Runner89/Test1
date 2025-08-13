b5a54bae0ce382cd0025904d71d27e8fa570d77c
Runner89 <alain.login@hotmail.com>
2025-08-13 11:59:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -501,7 +501,7 @@ def webhook():
     logs.append(f"Plaziere Market-Order mit {usdt_amount} USDT für {symbol} ({position_side})...")
     order_response = place_market_order(api_key, secret_key, symbol, float(usdt_amount), position_side)
     alarm_counter[botname] += 1
-    logs.append(firebase_speichere_ordergroesse(botname, usdt_amount, firebase_secret))
+    logs.append(firebase_speichere_ordergroesse(botname, saved_usdt_amounts[botname], firebase_secret))
     time.sleep(2)
     logs.append(f"Market-Order Antwort: {order_response}")
 
