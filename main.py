b1e93d83daa7fffa032e0a1d96176c81b615bffa
Runner89 <alain.login@hotmail.com>
2025-08-14 12:45:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -793,6 +793,8 @@ def webhook():
                 "Botname": botname,
                 "logs": logs
             })
+        
+        return "OK", 200
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
