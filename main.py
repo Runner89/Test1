ef8b01cad61d542aa3c362f905a3dc36213deb2e
Runner89 <alain.login@hotmail.com>
2025-08-14 07:51:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -531,7 +531,7 @@ def webhook():
     
         open_sell_orders_exist = False
 
-        if side2 == "long" and botname:
+        if side2 != "long" and botname:
             open_sell_orders_exist = True
         
         
